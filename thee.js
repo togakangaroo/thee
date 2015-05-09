cf9a497138cd4d404032e76de5169e581c890f92
George Mauer <gmauer@gmail.com>
2015-05-09 11:47:53 -0500

    remove dep on lodash

--- thee.js
+++ thee.js
@@ -1,13 +1,47 @@
-import _ from 'lodash'
-
 let thee = (config) =>
-	_.isFunction(config) 
+	isFunction(config) 
 		?  	function(...args) {
 				return config.apply(null, [this].concat(args));
 			}
-	: _.isObject(config)
-		? 	_.object(_.map(config, (propValue, propName) => [ propName, thee(propValue)] ))
+	: isObject(config)
+		? 	objectMap(config, (propValue, propName) => [ propName, thee(propValue)] )
 	//default
 		: config
 
 export default thee
+
+// is* functions just flat out ripped from lodash
+
+function isObject(value) {
+	var type = typeof value;
+	return type == 'function' || (!!value && type == 'object');
+}
+function isFunction(value) {
+	return typeof value == 'function' || false;
+}
+
+function map(obj, fn) {
+	const acc = [];
+	for(let propName in obj)
+		if(obj.hasOwnProperty(propName))
+			acc.push(fn.call(null, obj[propName], propName));
+	return acc;
+}
+
+function object(propNameVals) {
+	const obj = {};
+	for(let i in propNameVals)
+		obj[propNameVals[i][0]] = propNameVals[i][1];
+	return obj;
+}
+
+function objectMap(obj, fn) {
+	const res = {};
+	for(let propName in obj)
+		if(obj.hasOwnProperty(propName)) {
+			let propNameVals = fn(obj[propName], propName);
+			res[propNameVals[0]] = propNameVals[1];
+		}
+	return res;
+}
+
