58b97f9867d56da9f4a69ee5e2cc68d7e4176921
George Mauer <gmauer@gmail.com>
2015-05-09 17:21:38 -0500

    removed dead code

--- thee.js
+++ thee.js
@@ -34,21 +34,6 @@ function isFunction(value) {
 	return typeof value == 'function' || false;
 }
 
-function map(obj, fn) {
-	const acc = [];
-	for(let propName in obj)
-		if(obj.hasOwnProperty(propName))
-			acc.push(fn.call(null, obj[propName], propName));
-	return acc;
-}
-
-function object(propNameVals) {
-	const obj = {};
-	for(let i in propNameVals)
-		obj[propNameVals[i][0]] = propNameVals[i][1];
-	return obj;
-}
-
 function objectMap(obj, fn) {
 	const res = {};
 	for(let propName in obj)
